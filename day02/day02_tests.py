0834988759ed236d20f62fec4c5d8a80ff29a573
Godsmith <filip.lange@gmail.com>
2016-12-02 19:47:34 +0100

    day02 part 1

---
+++ day02/day02_tests.py
@@ -0,0 +1,46 @@
+from day02 import Keypad
+
+
+def test_add_tuples():
+    assert Keypad._add_lists([1, 3], [2, 4]) == [3, 7]
+
+
+def test_start_location():
+    keypad = Keypad()
+    assert keypad.current_digit == 5
+
+
+def test_move():
+    keypad = Keypad()
+    keypad.move_finger('U')
+    assert keypad.current_digit == 2
+
+    keypad = Keypad()
+    keypad.move_finger('R')
+    assert keypad.current_digit == 6
+
+
+def test_move_outside():
+    keypad = Keypad()
+    keypad.move_finger('U')
+    keypad.move_finger('U')
+    assert keypad.current_digit == 2
+
+    keypad.move_finger('L')
+    keypad.move_finger('L')
+    assert keypad.current_digit == 1
+
+
+def test_move_all():
+    keypad = Keypad()
+    keypad.move_all('UU')
+    assert keypad.current_digit == 2
+
+    keypad = Keypad()
+    keypad.move_all('UDLR')
+    assert keypad.current_digit == 5
+
+
+def test_to_code():
+    keypad = Keypad()
+    assert keypad.to_code(['ULL', 'RRDDD', 'LURDL', 'UUUUD']) == '1985'
