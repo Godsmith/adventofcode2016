f7b2268ea18f8208af58eab8c533f66754d0543e
Godsmith <filip.lange@gmail.com>
2016-12-07 20:15:01 +0100

    day07 part 2

--- day07/ipv7_string.py
+++ day07/ipv7_string.py
@@ -1,9 +1,16 @@
+from itertools import chain
+
+flatten = chain.from_iterable
+
 class Ipv7String:
     def __init__(self, string):
         self.string = string
         strings_to_the_left_of_brackets = string.split('[')
         self._strings_outside_brackets = [s.split(']')[-1] for s in strings_to_the_left_of_brackets]
         self._strings_inside_brackets = [s.split(']')[0] for s in strings_to_the_left_of_brackets][1:]
+
+        self._abas_outside_brackets = flatten(self._abas(s) for s in self._strings_outside_brackets)
+        self._abas_inside_brackets = flatten(self._abas(s) for s in self._strings_inside_brackets)
 
     def _contains_abba_outside_brackets(self):
         return any([self._contains_abba(s) for s in self._strings_outside_brackets])
@@ -19,5 +26,23 @@ class Ipv7String:
                 return True
         return False
 
+    @staticmethod
+    def _abas(s):
+        for i in range(len(s) - 2):
+            substring = s[i:i + 3]
+            if substring[0] == substring[2] and substring[0] != substring[1]:
+                yield substring
+        return False
+
+    @staticmethod
+    def _invert_aba(s):
+        return ''.join([s[1], s[0], s[1]])
+
     def supports_tls(self):
         return self._contains_abba_outside_brackets() and not self._contains_abba_inside_brackets()
+
+    def supports_ssl(self):
+        for aba in self._abas_outside_brackets:
+            if any([self._invert_aba(aba) in s for s in self._strings_inside_brackets]):
+                return True
+        return False
--- day07/test_ipv7_string.py
+++ day07/test_ipv7_string.py
@@ -28,3 +28,28 @@ def test_supports_tls():
     assert not Ipv7String('abcd[bddb]xyyx').supports_tls()
     assert not Ipv7String('aaaa[qwer]tyui').supports_tls()
     assert Ipv7String('ioxxoj[asdfgh]zxcvbn').supports_tls()
+
+
+def test_abas():
+    assert list(Ipv7String._abas('fdgabad')) == ['aba']
+    assert list(Ipv7String._abas('fdgbad')) == []
+    assert list(Ipv7String._abas('xyx')) == ['xyx']
+    assert list(Ipv7String._abas('xyxy')) == ['xyx', 'yxy']
+
+
+def test_abas_outside_brackets():
+    assert list(Ipv7String('aba[bab]xyz')._abas_outside_brackets) == ['aba']
+    assert list(Ipv7String('aba[bab]fdxyxdg')._abas_outside_brackets) == ['aba', 'xyx']
+    assert list(Ipv7String('aaa[bab]fdxxxdg')._abas_outside_brackets) == []
+
+
+def test_invert_aba():
+    assert Ipv7String._invert_aba('aba') == 'bab'
+
+
+def test_supports_ssl():
+    assert Ipv7String('aba[bab]xyz').supports_ssl()
+    assert not Ipv7String('xyx[xyx]xyx').supports_ssl()
+    assert not Ipv7String('sss[sss]abc').supports_ssl()
+    assert Ipv7String('aaa[kek]eke').supports_ssl()
+    assert Ipv7String('zazbz[bzb]cdb').supports_ssl()
