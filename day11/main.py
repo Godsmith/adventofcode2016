ee89a3c5e36c82378287000530142149bf6d977c
Godsmith <filip.lange@gmail.com>
2016-12-13 22:43:42 +0100

    day11 beginning part 2, it seems like only selecting good moves will get you stuck, so I have to start accepting less good moves as well.

--- day11/main.py
+++ day11/main.py
@@ -5,41 +5,35 @@ from day11.floor import Floor
 class NoGoodMovesException(Exception):
     pass
 
+
 def main():
-    initial_state = [Floor('F1', {'E', 'SG', 'SM', 'PG', 'PM'}),
+    # initial_state = [Floor('F1', {'E', 'SG', 'SM', 'PG', 'PM'}),
+    #                  Floor('F2', {'TG', 'RG', 'RM', 'CG', 'CM'}),
+    #                  Floor('F3', {'TM'}),
+    #                  Floor('F4')]
+    initial_state = [Floor('F1', {'E', 'EG', 'EM', 'DG', 'DM', 'SG', 'SM', 'PG', 'PM'}),
                      Floor('F2', {'TG', 'RG', 'RM', 'CG', 'CM'}),
                      Floor('F3', {'TM'}),
                      Floor('F4')]
-    facility = Facility(initial_state)
     moves = []
     excluded_states = set()
-    manual = False
-    while not facility.all_objects_on_fourth_floor():
+    while True:
+        facility = Facility.create(initial_state, moves)
         print()
         print(facility)
+        if facility.all_objects_on_fourth_floor():
+            break
         possible_moves = list(facility.possible_moves(excluded_states))
         for i, move in enumerate(possible_moves):
             print('%s: %s' % (i, move))
-        if manual:
-            ok = False
-            while not ok:
-                input_ = input('Total moves: %s. Select move: ' % len(moves))
-                try:
-                    moves.append(possible_moves[int(input_)])
-                    ok = True
-                except (IndexError, ValueError):
-                    print('Invalid input.')
-        else:
-            try:
-                move = best_move(possible_moves, facility)
-                moves.append(move)
-                print('Selected move: %s. Total moves: %s' % (move, len(moves)))
-            except NoGoodMovesException:
-                excluded_states.add(facility)
-                moves = moves[:-1]
-                print('No good moves, excluding facility.')
-
-        facility = Facility.create(initial_state, moves)
+        try:
+            move = best_move(possible_moves, facility)
+            moves.append(move)
+            print('Selected move: %s. Total moves: %s' % (move, len(moves)))
+        except NoGoodMovesException:
+            excluded_states.add(facility)
+            moves = moves[:-1]
+            print('No good moves, excluding facility.')
 
 
 def best_move(moves, facility):
