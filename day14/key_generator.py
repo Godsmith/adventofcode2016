2ac98e24c01bfec8d5a6d44d9848aaf8f5eadb06
Godsmith <filip.lange@gmail.com>
2016-12-19 21:40:12 +0100

    day14 next steps

--- day14/key_generator.py
+++ day14/key_generator.py
@@ -6,15 +6,38 @@ class KeyGenerator:
         self._salt = salt.encode('utf-8')
 
     def contains_triple(self, index):
+        return self._string_contains_triple(self._md5(index))
+
+    def is_key(self, index):
+        success, character = self.contains_triple(index)
+        if not success:
+            return False
+        return self._five_characters_in_a_row_in_next_thousand_indices(index, character)
+
+    def _five_characters_in_a_row_in_next_thousand_indices(self, index, c):
+        for i in range(index + 1, index + 1001):
+            md5 = self._md5(i)
+            success, _ = self._string_contains_sequence(md5, 5, character=c)
+            if success:
+                return True
+        return False
+
+    def _md5(self, index):
         m = hashlib.md5()
         m.update(self._salt)
         m.update(str(index).encode('utf-8'))
-        s = m.hexdigest()
-        return self._string_contains_triple(s)
+        return m.hexdigest()
 
-    @staticmethod
-    def _string_contains_triple(s):
+    @classmethod
+    def _string_contains_triple(cls, s):
+        return cls._string_contains_sequence(s, 3)
+
+    @classmethod
+    def _string_contains_sequence(cls, s, n, character=None):
         for i, c in enumerate(s[:-2]):
-            if c == s[i + 1] == s[i + 2]:
-                return True
-        return False
+            if len(set(s[i:i + n])) == 1:
+                if character is None:
+                    return True, c
+                elif character == c:
+                    return True, c
+        return False, None
--- day14/test_key_generator.py
+++ day14/test_key_generator.py
@@ -9,5 +9,20 @@ def keygen():
 
 
 def test_contains_triple(keygen):
-    assert not keygen.contains_triple(index=17)
-    assert keygen.contains_triple(index=18)
+    assert keygen.contains_triple(index=17) == (False, None)
+    assert keygen.contains_triple(index=18) == (True, '8')
+
+
+def test_string_contains_sequence(keygen):
+    assert (True, '8') == keygen._string_contains_sequence('88888', 5, '8')
+    assert (True, '8') == keygen._string_contains_sequence('a88888f', 5, '8')
+    assert (False, None) == keygen._string_contains_sequence('a8888f', 5, '8')
+
+
+def test_is_key_failure(keygen):
+    assert not keygen.is_key(index=17)
+    assert not keygen.is_key(index=18)
+
+
+def test_is_key_success(keygen):
+    assert keygen.is_key(index=39)
