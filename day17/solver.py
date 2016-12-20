70a45c3f5d7b40e0ef41ebebbc7a8571d002916d
Godsmith <filip.lange@gmail.com>
2016-12-20 23:24:30 +0100

    day17 part 2

--- day17/solver.py
+++ day17/solver.py
@@ -17,3 +17,20 @@ class Solver:
             paths_to_finish = [m.path for m in states_to_evaluate if m.room == (3, 3)]
             if len(paths_to_finish) > 0:
                 return paths_to_finish[0]
+
+    @property
+    def longest_path_length(self):
+        states_to_evaluate = [Maze(passcode=self._passcode)]
+        steps = 0
+        max_steps = 0
+        while len(states_to_evaluate) > 0:
+            steps += 1
+            states_to_evaluate = list(flatten([m.adjacent_states for m in states_to_evaluate]))
+            if (3, 3) in [m.room for m in states_to_evaluate]:
+                max_steps = steps
+            states_to_evaluate = [m for m in states_to_evaluate if m.room != (3, 3)]
+        return max_steps
+
+
+print(Solver('pslxynzg').shortest_path)
+print(Solver('pslxynzg').longest_path_length)
--- day17/test_solver.py
+++ day17/test_solver.py
@@ -5,3 +5,9 @@ def test_shortest_path():
     assert Solver('ihgpwlah').shortest_path == 'DDRRRD'
     assert Solver('kglvqrro').shortest_path == 'DDUDRLRRUDRD'
     assert Solver('ulqzkmiv').shortest_path == 'DRURDRUDDLLDLUURRDULRLDUUDDDRR'
+
+
+def test_longest_path_length():
+    assert Solver('ihgpwlah').longest_path_length == 370
+    assert Solver('kglvqrro').longest_path_length == 492
+    assert Solver('ulqzkmiv').longest_path_length == 830
