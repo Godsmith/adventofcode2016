8b8204ded2bcd15351c42ef20a56f2fa31e6fc4d
Godsmith <filip.lange@gmail.com>
2016-12-21 22:10:54 +0100

    day18

--- day18/room.py
+++ day18/room.py
@@ -5,3 +5,27 @@ class Room:
             return '^'
         else:
             return '.'
+
+    @staticmethod
+    def make_row(row):
+        extended_row = '.' + row + '.'
+        output = []
+        for i in range(1, len(row) + 1):
+            tile_input = extended_row[i - 1:i + 2]
+            output.append(Room.make_tile(tile_input))
+        return ''.join(output)
+
+    @staticmethod
+    def make_rows(row, n):
+        output = [row]
+        while len(output) < n:
+            output.append(Room.make_row(output[-1]))
+        return output
+
+    @staticmethod
+    def count_safe_tiles(rows):
+        return sum(s.count('.') for s in rows)
+
+
+print(Room.count_safe_tiles(Room.make_rows(
+    '.^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....', 400000)))
--- day18/test_room.py
+++ day18/test_room.py
@@ -11,3 +11,16 @@ def test_make_tile():
     assert Room.make_tile('.^.') == '.'
     assert Room.make_tile('^^^') == '.'
     assert Room.make_tile('^.^') == '.'
+
+
+def test_make_row():
+    assert Room.make_row('..^^.') == '.^^^^'
+    assert Room.make_row('.^^^^') == '^^..^'
+
+
+def test_make_rows():
+    assert Room.make_rows('..^^.', 3) == ['..^^.', '.^^^^', '^^..^']
+
+
+def test_count_safe_tiles():
+    assert Room.count_safe_tiles(Room.make_rows('..^^.', 3)) == 6
