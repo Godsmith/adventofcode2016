c15a9d9f42ac1bc87ee12d2077fdfef77b2905fd
Godsmith <filip.lange@gmail.com>
2016-12-25 21:40:47 +0100

    day21 part 1

--- day21/main.py
+++ day21/main.py
@@ -1,5 +1,5 @@
 from day21.scrambler import Scrambler
 
 with open('input.txt') as f:
-    operations = f.readlines()
+    operations = [line[:-1] for line in f.readlines() if len(line) > 0]
     print(Scrambler.do_all(operations, 'abcdefgh'))
